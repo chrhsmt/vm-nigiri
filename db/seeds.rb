98c6f3b7f10aa04ee04d52db27739c3bb976391a
chrhsmt <chr@chrhsmt.com>
2015-01-31 17:31:17 +0900

    add ip_ranege and migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/../models")
 require "machine"
 
-::Machine.create!({name: "syake2", disk_size: 300000, memory: 60000, ip: "192.168.0.30"})
+::Machine.destroy_all
+::Machine.create!({name: "syake2", disk_size: 300000, memory: 60000, ip: "192.168.0.30", ip_range: "192.168.0.96/28"})  unless ::Machine.where(name: "syake2").first
+::Machine.create!({name: "syake3", disk_size: 300000, memory: 60000, ip: "192.168.0.40", ip_range: "192.168.0.112/28"}) unless ::Machine.where(name: "syake3").first
+::Machine.create!({name: "syake4", disk_size: 300000, memory: 60000, ip: "192.168.0.50", ip_range: "192.168.0.128/28"}) unless ::Machine.where(name: "syake4").first
